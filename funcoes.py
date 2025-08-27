bfbe27f43a470f8b3c4484faa174dbb1233c97e0
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-08-27 15:33:13 -0300

    Create funcoes.py

---
+++ funcoes.py
@@ -0,0 +1,7 @@
+# funcoes.py
+
+# Este arquivo será preenchido colaborativamente pelos alunos.
+# Cada aluno deve adicionar sua função abaixo, com comentários explicativos.
+
+def exemplo():
+    print("Função de exemplo. Substitua por sua função!")
