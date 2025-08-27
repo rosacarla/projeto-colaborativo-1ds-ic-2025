91345a0ad81a6ab21fa174e62f1b03b72f2f76ae
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-08-27 15:59:35 -0300

    Create funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+// funcoes.js
+
+// Este arquivo será preenchido colaborativamente pelos alunos.
+// Cada aluno deve adicionar sua função abaixo, com comentários explicativos.
+
+function exemplo() {
+  console.log("Função de exemplo. Substitua por sua função!");
+}
